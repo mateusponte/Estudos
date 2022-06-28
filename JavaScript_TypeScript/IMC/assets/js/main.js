360e9eb034bc02b0fbcee4874c2124da014d2aa7
mateusponte <mateus98ponte@gmail.com>
2022-06-28 14:36:10 -0300

    atualizações dos treinos dia 28/06

--- JavaScript_TypeScript/IMC/assets/js/main.js
+++ JavaScript_TypeScript/IMC/assets/js/main.js
@@ -1,7 +1,14 @@
 function calcular(){
+    
     var peso = document.getElementById("Peso").value
     var altura = document.getElementById("Altura").value
 
-    imc = peso/(altura*altura);
-    alert(imc)
+    if(altura == null || peso == null || altura == 0 || peso == 0){
+        alert('Não é possível calcular o IMC, digite algum valor!!!!')
+    } else{
+        imc = peso/(altura*altura);
+        alert(imc)
+    }
+
+
 }
